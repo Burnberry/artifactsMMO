86647c1634a6ca39668295d2c92510f1883cc777
Noppe <CoinCabal@outlook.com>
2025-02-26 23:52:57 +0100

    basic template

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from helpers import *
 from player import Player
 from characters import *
 
-players = [Noppe(), Rubius(), Leandra(), Hella(), Pebbleboy]
+players = [Noppe(), Rubius(), Leandra(), Hella(), Pebbleboy()]
 for player in players:
     player.start_thread()
 
